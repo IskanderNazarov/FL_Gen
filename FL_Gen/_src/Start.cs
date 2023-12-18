fe24456b49e93074a9a7745e1a3b452bf504dad3
Alexander <fighter.nazarov@gmail.com>
2023-12-18 07:35:17 +0400

    reviewing

--- FL_Gen/_src/Start.cs
+++ FL_Gen/_src/Start.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
 internal class Start {
+
+    private ComplexityData[] complexityData = {
+        new ComplexityData {complexity = 1, levelsCount = 3, flasksCount = 3},
+        new ComplexityData {complexity = 2, levelsCount = 3, flasksCount = 4},
+        new ComplexityData {complexity = 3, levelsCount = 2, flasksCount = 5},
+        new ComplexityData {complexity = 4, levelsCount = 2, flasksCount = 5},
+        new ComplexityData {complexity = 5, levelsCount = 2, flasksCount = 8},
+        new ComplexityData {complexity = 6, levelsCount = 2, flasksCount = 11},
+    };
+
+    private int levelNumber = 1;
     public static void Main(string[] args) {
         new Start().StartGenerating();
     }
 
     private void StartGenerating() {
-        var level = new LevelCreator().Create(5, 4);
-        Console.WriteLine(level);
+        foreach (var data in complexityData) {
+            for (var i = 0; i < data.levelsCount; i++) {
+                var level = new LevelCreator().Create(data.flasksCount, 4);
+                Utils.WriteLevelToFile(level.ToString(), levelNumber, data.complexity);
+
+                levelNumber++;
+            }
+        }
+        
     }
 }
